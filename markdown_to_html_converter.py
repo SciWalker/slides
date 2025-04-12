69cfc384e838083c37c8415c941f7779e3ede13a
ElijahWong <wongwenwei@live.com>
2025-04-12 23:08:23 +0800

    Update markdown converter to process bodily resurrection slides

--- markdown_to_html_converter.py
+++ markdown_to_html_converter.py
@@ -130,7 +130,7 @@ def insert_title_into_the_html(content_html,main_title):
     with open(f'out/{md_name}.html', 'w', encoding='utf-8') as file:
         file.write(final_html)
 
-md_name="06042025_second_coming"
+md_name="13042025_bodily_resurrection"
 # Process the markdown file to get the HTML content
 content_html,main_title = process_markdown_file(f'out/{md_name}.md')
 # Insert the content into the template and save the output
